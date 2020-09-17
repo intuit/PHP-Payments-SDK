a4cfdf67294b705301f8c602d7f772c5f7611e71
abisalehalliprasan <anil_kumar3@intuit.com>
2020-09-17 12:18:58 -0700

    Feature : Ignore Unknown Fields

--- src/Modules/Context.php
+++ src/Modules/Context.php
@@ -30,8 +30,6 @@ class Context
                         $this->{$name} = $value;
                     }
                 }
-            } else {
-                throw new \RuntimeException("Property name: [" . $name . "] is not a valid field for: [" . get_class($this) . "]. Please check your keys.");
             }
         }
     }
--- src/Modules/ZeroDollarVerification.php
+++ src/Modules/ZeroDollarVerification.php
@@ -22,8 +22,6 @@ class ZeroDollarVerification
                         $this->{$name} = $value;
                     }
                 }
-            } else {
-                throw new \RuntimeException("Property name: [" . $name . "] is not a valid field for: [" . get_class($this) . "]. Please check your keys.");
             }
         }
     }
